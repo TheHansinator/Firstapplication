d5d3be8b0c5c7dad3518fd865e3e82be66a3998e
TheHansinator <38115936+TheHansinator@users.noreply.github.com>
2018-04-06 09:35:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hansinator/firstapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.hansinator.firstapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void websiteClick(View view){
+        Intent I = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.dk/?gws_rd=cr&dcr=0&ei=IL-nWpmiLs72kwXIk4GIDQ"));
+        startActivity(I);
+    }
+
+    public void phoneClick(View view) {
+        Intent I = new Intent(Intent.ACTION_DIAL,
+                Uri.parse("tel:23733883"));
+        startActivity(I);
+    }
+
+    public void mapClick(View view){
+        Intent I = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("geo:57.012494,9.991073"));
+        startActivity(I);
+    }
+}
